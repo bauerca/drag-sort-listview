af47d8ad2719bbd2c8d7e794810817472d37d21f
Carl Bauer <heycosmo@gmail.com>
2012-10-30 16:48:36 -0700

    Update API with DragSortCursorAdapter stuff

--- reference/lists.js
+++ reference/lists.js
@@ -1,13 +1,18 @@
 var DATA = [
       { id:0, label:"com.mobeta.android.dslv", link:"com/mobeta/android/dslv/package-summary.html", type:"package" },
       { id:1, label:"com.mobeta.android.dslv.DragSortController", link:"com/mobeta/android/dslv/DragSortController.html", type:"class" },
-      { id:2, label:"com.mobeta.android.dslv.DragSortListView", link:"com/mobeta/android/dslv/DragSortListView.html", type:"class" },
-      { id:3, label:"com.mobeta.android.dslv.DragSortListView.DragListener", link:"com/mobeta/android/dslv/DragSortListView.DragListener.html", type:"class" },
-      { id:4, label:"com.mobeta.android.dslv.DragSortListView.DragScrollProfile", link:"com/mobeta/android/dslv/DragSortListView.DragScrollProfile.html", type:"class" },
-      { id:5, label:"com.mobeta.android.dslv.DragSortListView.DragSortListener", link:"com/mobeta/android/dslv/DragSortListView.DragSortListener.html", type:"class" },
-      { id:6, label:"com.mobeta.android.dslv.DragSortListView.DropListener", link:"com/mobeta/android/dslv/DragSortListView.DropListener.html", type:"class" },
-      { id:7, label:"com.mobeta.android.dslv.DragSortListView.FloatViewManager", link:"com/mobeta/android/dslv/DragSortListView.FloatViewManager.html", type:"class" },
-      { id:8, label:"com.mobeta.android.dslv.DragSortListView.RemoveListener", link:"com/mobeta/android/dslv/DragSortListView.RemoveListener.html", type:"class" },
-      { id:9, label:"com.mobeta.android.dslv.SimpleFloatViewManager", link:"com/mobeta/android/dslv/SimpleFloatViewManager.html", type:"class" }
+      { id:2, label:"com.mobeta.android.dslv.DragSortCursorAdapter", link:"com/mobeta/android/dslv/DragSortCursorAdapter.html", type:"class" },
+      { id:3, label:"com.mobeta.android.dslv.DragSortListView", link:"com/mobeta/android/dslv/DragSortListView.html", type:"class" },
+      { id:4, label:"com.mobeta.android.dslv.DragSortListView.DragListener", link:"com/mobeta/android/dslv/DragSortListView.DragListener.html", type:"class" },
+      { id:5, label:"com.mobeta.android.dslv.DragSortListView.DragScrollProfile", link:"com/mobeta/android/dslv/DragSortListView.DragScrollProfile.html", type:"class" },
+      { id:6, label:"com.mobeta.android.dslv.DragSortListView.DragSortListener", link:"com/mobeta/android/dslv/DragSortListView.DragSortListener.html", type:"class" },
+      { id:7, label:"com.mobeta.android.dslv.DragSortListView.DropListener", link:"com/mobeta/android/dslv/DragSortListView.DropListener.html", type:"class" },
+      { id:8, label:"com.mobeta.android.dslv.DragSortListView.FloatViewManager", link:"com/mobeta/android/dslv/DragSortListView.FloatViewManager.html", type:"class" },
+      { id:9, label:"com.mobeta.android.dslv.DragSortListView.RemoveListener", link:"com/mobeta/android/dslv/DragSortListView.RemoveListener.html", type:"class" },
+      { id:10, label:"com.mobeta.android.dslv.ResourceDragSortCursorAdapter", link:"com/mobeta/android/dslv/ResourceDragSortCursorAdapter.html", type:"class" },
+      { id:11, label:"com.mobeta.android.dslv.SimpleDragSortCursorAdapter", link:"com/mobeta/android/dslv/SimpleDragSortCursorAdapter.html", type:"class" },
+      { id:12, label:"com.mobeta.android.dslv.SimpleDragSortCursorAdapter.CursorToStringConverter", link:"com/mobeta/android/dslv/SimpleDragSortCursorAdapter.CursorToStringConverter.html", type:"class" },
+      { id:13, label:"com.mobeta.android.dslv.SimpleDragSortCursorAdapter.ViewBinder", link:"com/mobeta/android/dslv/SimpleDragSortCursorAdapter.ViewBinder.html", type:"class" },
+      { id:14, label:"com.mobeta.android.dslv.SimpleFloatViewManager", link:"com/mobeta/android/dslv/SimpleFloatViewManager.html", type:"class" }
 
     ];
